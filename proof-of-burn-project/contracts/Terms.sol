bfcd2688b033d8f994337a74e4484eba8af884cd
Sean Yang <xsvfat@gmail.com>
2021-08-27 20:00:18 +0900

    updateChainlink oracle and terms to call a single url

--- proof-of-burn-project/contracts/Terms.sol
+++ proof-of-burn-project/contracts/Terms.sol
@@ -2,22 +2,21 @@
 
 pragma solidity ^0.8.0;
 import "./ownable.sol";
+import "./ChainLinkOracle.sol";
+import "./Helper.sol";
 
-contract Terms is Ownable {
+contract Terms is Ownable, Helper {
 
-    event NewEntry(address _address, uint betAmount, uint endDate);
+    event NewEntry(address _address, uint betAmount, uint endDate, uint stepsGoal);
     event resultsReceived(address _address, uint returnedAmount, bool outcome, uint avgHistoricalSteps);
-
     
-    // the number of days each contest will run for and number of steps above avg they need to take
-    uint32 public daysForContest = 7 days;
-    uint64 public stepsRequiredAboveHistoricalAvg = 1000;
-
 
     struct Entry {
-        uint betAmount; //amount participant bets
+        uint256 betAmount; //amount participant bets
         uint32 startDate; // when participant made bet 
         string accessToken;
+        uint256 stepsGoal;
+        uint16 numberOfDays;
         bool activeEntry;
     }
     // This declares a state variable that
@@ -27,29 +26,30 @@ contract Terms is Ownable {
     // TODO mapping of accessToken to addresses? We need prevent multiple addresses from using the same access token
 
 
-    function makeEntry(string _accessToken) external payable {
+    function makeEntry(string _accessToken,uint256 stepsGoal, uint16 numberOfDays) external payable {
         // Check that user does not have a active entry
         // If not, add an entry and add mapping to entries
         require(!entries[msg.sender].activeEntry, "You already have an active entry, only one active entry per participant!");
         require(msg.value > 0, "Please include a bet amount");
-        
-        entries[msg.sender] = Entry(msg.value, uint32(now), _accessToken, true);
+        uint32 currentDate = uint32(now);
+        entries[msg.sender] = Entry(msg.value, currentDate, _accessToken, stepsGoal, numberOfDays, true);
+        emit NewEntry(_address, betAmount, currentDate + numberOfDays, stepsGoal);
     }
 
-    function clearEntry (address _address) private {
 
+    function _buildOuraRequestUrl (uint32 _startDate, uint32 _endDate, string _accessToken, uint256 _stepsGoal) private returns(string){
+        string requestUrl = "https://ea4ank8od6.execute-api.us-east-1.amazonaws.com/default/getOuraRingDataResults?startTimestamp=" + uint2str(_startDate) + "&token=" + _accessToken + "&stepsGoal=" + uint2str(_stepsGoal) + "&endTimestamp=" + uint2str(_endDate);
+        return requestUrl;
     }
 
-    function checkResults() external (){
-        constant entry = entries[msg.sender];
+    function checkResults() external returns(uint256){
+        address entry = entries[msg.sender];
         // if days for contest + start date is less than today, let them know the contest isn't over yet
         // if days for contest + start date is past the current time, make a call to oracle
-        if (entry.startDate + daysForContest < uint32(now)){
-
-        } else {
-            //contest is over, make call to oracle. Clear entry
-
-        }
+        require(entry.startDate + entry.numberOfDays < uint32(now));
+        string url = _buildOuraRequestUrl(entries.startDate, entries.startDate + entries.numberOfDays, entries.accessToken, entries.stepsGoal);
+        // delete entries[msg.sender];
+        
     }
 
 }
